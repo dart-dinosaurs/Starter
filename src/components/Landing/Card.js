7775ee478574ac7fb3beef6968c0e2c9163d201f
skeshavaa <s.keshavaa@gmail.com>
2020-01-25 23:37:31 -0500

    finalized landing

--- src/components/Landing/Card.js
+++ src/components/Landing/Card.js
@@ -3,7 +3,6 @@ import React from 'react'
 import LandingStyles from '@styles/Landing/LandingCard.module.scss'
 
 const Card = (props) => {
-    console.log(props)
     return(
         <div className={LandingStyles.container}>
             <div className={LandingStyles.imgContainer}>
